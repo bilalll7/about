d0d6195063d571b2b3f0c27e6b5b96102fbf9052
bilalll7 <albilalbintang909@gmail.com>
2023-11-17 14:39:25 +0700

    first commit

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function index() {
+  return (
+    <>
+      <div className="about">
+        <div className="about-card">
+          <h2>About</h2>
+          <p className="p-first">
+            Hello! I’m <b>Albilal</b> Bintang Iskandar, and I’m a Junior Web
+            Developer who loves coding and learning new things.
+          </p>
+          <p>
+            I’ve been programming since 2020, and I have never stopped
+            programming since then. I might even say I find it fun to program -
+            most of the time. I attended vocational high school with major in
+            Software Engineering, and I continued my study in Indonesian
+            Computer University with major in Informatics Engineering. During my
+            time as a student, I’ve been involved as freelance projects, and
+            internships. I’ve also been involved in several competitions. More
+            on that later!
+          </p>
+          <p>
+            While I know several programming languages, I’m most comfortable
+            with JavaScript and PHP. Other languages that I’m familiar with are
+            PHP, Java and Python. I’m also familiar with several frameworks and
+            libraries, such as React, Vue, Vite, Laravel, Bootstrap, and
+            Tailwind. Currenly, I’m exploring Dart - which is a super cool
+            language. They have a lot of interesting concepts! Sometimes I wish
+            I could use them in my daily job without having to worry about
+            confusing my teammates.
+          </p>
+          <p>
+            Okay, enough talk about programming! Let’s get into my interests. I
+            also play games. I play several MOBA games, for example Mobile
+            Legends, Wild Rift and DOTA 2. But I focus more on playing Mobile
+            Legends. I also had time to participate in the mobile legends pro
+            scene up to tier III. I also had the opportunity to jockey for
+            mobile legends accounts, tournaments and earn income from there.
+            I've spent over 10,000 hours playing it, and I'm still not bored.
+            There are many overhaul mods that give the game hundreds of hours
+            more play time. Unfortunately (or rather, fortunately), I don't have
+            much time left to play games at the moment.
+          </p>
+          <p>
+            Wow, that’s a lot of things about me! I hope you enjoy reading it.
+            If you want to know more about me, feel free to contact me through
+            my social media or email. I’m always open to new opportunities and
+            new friends. Have a nice day~
+          </p>
+          <p>Cheers!</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default index;
